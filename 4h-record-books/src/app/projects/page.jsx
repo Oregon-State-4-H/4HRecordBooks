91d4b94b4a96788da2fa0243741b59b27370ad14
michellehuyen <nguyemi4@oregonstate.edu>
2024-01-16 19:49:03 -0800

    minor changes to positioning of text

--- 4h-record-books/src/app/projects/page.jsx
+++ 4h-record-books/src/app/projects/page.jsx
@@ -3,7 +3,7 @@ import classes from './styles.module.css';
 export default function Projects() {
   return (
     <main>
-      <h1>My Projects</h1>
+      <h1><b>My Projects</b></h1>
     </main>
   );
 }
