4d10a21a419ad9e5227fe5c66e6e6b39658ae464
michellehuyen <nguyemi4@oregonstate.edu>
2024-01-17 17:50:23 -0800

    starting accounts page

--- 4h-record-books/src/app/account/page.jsx
+++ 4h-record-books/src/app/account/page.jsx
@@ -3,7 +3,16 @@ import classes from './styles.module.css';
 export default function Account() {
   return (
     <main>
-      <h1>My Account</h1>
+      <h1><b>My Account</b></h1>
+      <div className="card-grid">
+
+        <div className= { classes.acc_card }>
+          <div className={ classes.acc_card_header }>
+            <div className={ classes.acc_card_title }>Settings</div>
+          </div>
+        </div>
+
+      </div>
     </main>
   );
 }
