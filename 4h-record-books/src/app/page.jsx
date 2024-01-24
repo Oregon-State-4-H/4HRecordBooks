f8d9720d942b72d806dce2843ac4ea8881503ad4
michellehuyen <nguyemi4@oregonstate.edu>
2024-01-24 13:16:25 -0800

    made changes based on review

--- 4h-record-books/src/app/page.jsx
+++ 4h-record-books/src/app/page.jsx
@@ -1,96 +1,88 @@
+function Card(props) {
+  return (
+      <div className="card">
+          {props.children}
+      </div>
+  )
+}
+
 export default function Home() {
   return (
     <main>
       <h1><b>Home</b></h1>
       <div className="card-grid">
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://yt3.googleusercontent.com/7jw2dHehoxZZWyHI5FqQr7KMJSzdTBfzuJomE-BmXLKQeBeFGTpv4kYCsc2IeQeaHt1X33yZQg=s900-c-k-c0x00ffffff-no-rj"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://yt3.googleusercontent.com/7jw2dHehoxZZWyHI5FqQr7KMJSzdTBfzuJomE-BmXLKQeBeFGTpv4kYCsc2IeQeaHt1X33yZQg=s900-c-k-c0x00ffffff-no-rj" />
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h">4-H Youth Development</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
+        <Card>
             <div className="e-card-img">
               <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2018-10/20180706120533.jpg?itok=QJ6NuphZ"></img>
             </div>
-          </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/program/all/4h/events">Events</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://www.4-h.org/wp-content/uploads/2022/11/07104850/E_-Branded-element-1.png"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://www.4-h.org/wp-content/uploads/2022/11/07104850/E_-Branded-element-1.png"></img>
           </div>
           <div className="e-card-content">
-            <a href="https://extension.oregonstate.edu/program/all/4h/local-programs">Find Your Local 4-H Youth Development Program</a>
+            <a href="https://extension.oregonstate.edu/program/all/4h/local-programs">Local 4-H Program Finder</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://4-h.org/wp-content/uploads/2023/02/02104805/323-cutout.jpg"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://4-h.org/wp-content/uploads/2023/02/02104805/323-cutout.jpg"></img>
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h/about-4-h">About 4-H</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2022-08/group-welcome-2.jpg?itok=1eoDV0NG"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2022-08/group-welcome-2.jpg?itok=1eoDV0NG"></img>
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h/4-h-summer-camps">4-H Summer Camps</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2018-10/recordbook.jpg?itok=N5ojWdV5"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2018-10/recordbook.jpg?itok=N5ojWdV5"></img>
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h/4-h-projects">4-H Projects</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2021-08/volunteer-b.png?itok=FBN8rSHC"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2021-08/volunteer-b.png?itok=FBN8rSHC"></img>
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h/volunteer-resources-0">Volunteer Resources</a>
           </div>
-        </div>
+        </Card>
 
-        <div className="e-card">
-          <div className="e-card-header">
-            <div className="e-card-img">
-              <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2021-07/4h-org4htexaseditshires-6220v2.jpg?itok=Q6bFQ3iN"></img>
-            </div>
+        <Card>
+          <div className="e-card-img">
+            <img src="https://extension.oregonstate.edu/sites/default/files/styles/full/public/images/2021-07/4h-org4htexaseditshires-6220v2.jpg?itok=Q6bFQ3iN"></img>
           </div>
           <div className="e-card-content">
             <a href="https://extension.oregonstate.edu/4h/support-4-h">Support 4-H</a>
           </div>
-        </div>
+        </Card>
 
       </div>
     </main>
