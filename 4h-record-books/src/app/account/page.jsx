79e2b59f07ba8a682b827dd68a23d8c31b2fa489
michellehuyen <nguyemi4@oregonstate.edu>
2024-01-29 13:23:14 -0800

    creating one big card

--- 4h-record-books/src/app/account/page.jsx
+++ 4h-record-books/src/app/account/page.jsx
@@ -1,15 +1,22 @@
 import classes from './styles.module.css';
 
+function Card(props) {
+  return (
+      <div className="card">
+          {props.children}
+      </div>
+  )
+}
+
 export default function Account() {
   return (
     <main>
       <h1><b>My Account</b></h1>
       <div className="card-grid">
 
-        <div className= { classes.acc_card }>
-          <div className={ classes.acc_card_header }>
-            <div className={ classes.acc_card_title }>Settings</div>
-          </div>
+        <div>
+          {/* <div className={ classes.acc_card_header }>Settings</div> */}
+          <button type="submit" id="btn">Settings</button>
         </div>
 
       </div>
