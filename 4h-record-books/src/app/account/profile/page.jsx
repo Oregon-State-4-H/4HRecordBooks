8b6e1ef0ef2d75f75219814201a95442e8d814ba
michellehuyen <nguyemi4@oregonstate.edu>
2024-02-05 11:02:12 -0800

    fixed alignment for edit profile pg

--- 4h-record-books/src/app/account/profile/page.jsx
+++ 4h-record-books/src/app/account/profile/page.jsx
@@ -18,17 +18,20 @@ export default function Profile() {
     return (
         <main>
             <h1><b>Edit Profile</b></h1>
-            <form>
+            <form className={classes.profileForm}>
                 <label className={classes.label}>
                     Name
+                    {/* <div className={classes.title}>Name</div> */}
                     <input className={classes.textInputBox} type="text" value={name} onChange={event => {setName(event.target.value)}} placeholder="Demo User" />
                 </label>
                 <label className={classes.label}>
                     Email
+                    {/* <div className={classes.title}>Email</div> */}
                     <input className={classes.textInputBox} type="email" value={email} onChange={event => {setEmail(event.target.value)}} placeholder="demouser@gmail.com" />
                 </label>
                 <label className={classes.label}>
                     County
+                    {/* <div className={classes.title}>County</div> */}
                     <input className={classes.textInputBox} type="text" value={county} onChange={event => {setCounty(event.target.value)}} placeholder="Benton" />
                 </label>
             </form>
---
+++ 4h-record-books/src/app/account/setting/page.jsx
@@ -0,0 +1,7 @@
+export default function Setting() {
+    return (
+        <main>
+            <h1><b>Notificaton Settings</b></h1>
+        </main>
+    )
+}
