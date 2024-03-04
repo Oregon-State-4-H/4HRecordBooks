f95a823f3403f7e795ff4b92e46c3317db2708f4
michellehuyen <nguyemi4@oregonstate.edu>
2024-03-04 15:57:02 -0800

    created add info modal

--- 4h-record-books/src/app/dashboard/resume/section/page.jsx
+++ 4h-record-books/src/app/dashboard/resume/section/page.jsx
@@ -3,20 +3,19 @@
 import { useState } from 'react';
 import ActionBar from '@/app/components/ActionBar';
 import styles from './styles.module.css';
-import { CiEdit } from "react-icons/ci";
 import { IoMdAdd } from "react-icons/io";
 
 import demoData from "../../../demoData.json"
 
-function TableCard({ title, data, headers }) {
+function TableCard({ title, data, headers, handleClick }) {
   return (
     <>
       <div className={styles.sectionHeader}>
         <span className={styles.sectionTitle}>{title}</span>
-        <div className={styles.addInfoContainer}>
+        <button className={styles.addInfoContainer} onClick={handleClick}>
           <IoMdAdd />
           <span id={styles.addInfo}>Add Info</span>
-        </div>
+        </button>
       </div>
 
       <table className={styles.table}>
@@ -50,47 +49,42 @@ function TableCard({ title, data, headers }) {
   )
 }
 
-function FormCard(props) {
-  var title = props.title;
-  var children = props.children;
-
+function FormCard({ onClose, children }) {
   return (
-    <div className={styles.formCard}>
-      <div className={styles.sectionHeader}>
-        <span className={styles.sectionTitle}>{title}</span>
-        <div className={styles.editInfoContainer}>
-          <CiEdit />
-          <span id={styles.editInfo}>Edit Info</span>
+    <div className={styles.overlay}>
+      <div className={styles.formCard}>
+        <button id={styles.closeBtn} onClick={onClose}>X</button>
+        <div className={styles.children}>
+          {children}
         </div>
+        <button type="submit" className={styles.submitBtn}>Submit</button>
       </div>
-      <div className={styles.children}>
-        {children}
-      </div>
-      <button type="submit" className={styles.submitBtn}>Submit</button>
     </div>
   )
 }
 
 function StringCard(props) {
   var text = props.text;
-  var name = props.name;
+  // var name = props.name;
 
   return (
       <label className={styles.label}>
           {text}
-          <input className={styles.textInputBox} type="text" placeholder={name} />
+          <input className={styles.textInputBox} type="text" />
+          {/* <input className={styles.textInputBox} type="text" placeholder={name} /> */}
       </label>
   )
 }
 
 function TextAreaCard(props) {
   var text = props.text;
-  var string = props.string;
+  // var string = props.string;
 
   return (
       <label className={styles.label}>
           {text}
-          <textarea className={styles.textInputBox} type="text" placeholder={string} />
+          <textarea className={styles.textInputBox} type="text" />
+          {/* <textarea className={styles.textInputBox} type="text" placeholder={string} /> */}
       </label>
   )
 }
@@ -129,40 +123,84 @@ function DropdownCard(props) {
   )
 }
 
+function addInfo3Columns(closeModal, col2, col3) {
+  return (
+    <FormCard onClose={closeModal}>
+      <StringCard text="Year" />
+      <TextAreaCard text={col2} />
+      {col3 && <TextAreaCard text={col3} />}
+    </FormCard>
+  )
+}
+
+function addInfo4Columns(closeModal, col2, col3, col4, col5) {
+  return (
+    <FormCard onClose={closeModal}>
+      <StringCard text="Year" />
+
+      <TextAreaCard text={col2} />
+      {col5 && <TextAreaCard text={col5} />}
+      
+      <NumberCard text={col3} />
+      <NumberCard text={col4} />
+    </FormCard>
+  )
+}
+
+function addInfoDropDownSections(closeModal, activity, text, arr) {
+  return (
+    <FormCard onClose={closeModal}>
+      <StringCard text="Year" />
+
+      <StringCard text={activity} />
+      <StringCard text={text} />
+
+      <DropdownCard options={arr} />
+    </FormCard>
+  )
+}
+
 function section1() {
   const headers = ["Year", "Grade", "Name of Club/Group", "Number in Club/Group", "Club/Group Leader or Advisor", "Meetings Held", "Meetings Attended"]
   const data = demoData.section1
+  const [showFormCard, setShowFormCard] = useState(false);
     
   return (
-    <TableCard title="4-H Involvement" data={data} headers={headers} />
-    // <FormCard title="4-H Involvement">
-    //   <StringCard text="Year" name="2012-13" />
-    //   <NumberCard text="Grade" number="9" />
-      
-    //   <StringCard text="Name of Club/Group" name="Oak Grove Clothing Club" />
-      
-    //   <NumberCard text="Number in Club/Group" number="9" />
-      
-    //   <StringCard text="Club/Group Leader or Advisor" name="Mrs. L. Jones" />
-      
-    //   <NumberCard text="Meetings Held" number="15" />
-    //   <NumberCard text="Meetings Attended" number="12" />
-    // </FormCard>
+    <>
+      <TableCard title="4-H Involvement" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && (
+        <FormCard onClose={() => setShowFormCard(false)}>
+          <StringCard text="Year" />
+          <NumberCard text="Grade" />
+          
+          <StringCard text="Name of Club/Group" />
+          
+          <NumberCard text="Number in Club/Group" />
+          
+          <StringCard text="Club/Group Leader or Advisor" />
+          
+          <NumberCard text="Meetings Held" />
+          <NumberCard text="Meetings Attended" />
+        </FormCard>
+      )}
+    </>
   )
 }
 
 function section2() {
   const headers = ["Year", "Name of Project/Unit", "Project Size or Scope"]
   const data = demoData.section2
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="4-H Project/Program" data={data} headers={headers} />
-    // <FormCard title="4-H Project/Program">
-    //   <StringCard text="Year" name="2012-13" />
-      
-    //   <StringCard text="Name of Project/Unit" name="Clothing" />
-    //   <TextAreaCard text="Project Size or Scope" string="1 apron, 2 dresses" />
-    // </FormCard>
+    <>
+      <TableCard title="4-H Project/Program" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo3Columns(closeModal, "Name of Project/Unit", "Project Size or Scope")}
+    </>
   )
 }
 
@@ -170,150 +208,176 @@ function section3() {
   const section3Arr = ['Local/Club', 'County', 'Regional', 'State', 'National', 'International'];
   const headers = ["Year", "Kind of Activity", "Things I learned", "Region"]
   const data = demoData.section3
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="4-H Activities/Events" data={data} headers={headers} />
-    // <FormCard title="4-H Activities/Events">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <StringCard text="Kind of Activity" name="Record Keeping Clinic" />
-    //   <StringCard text="Things I Learned" name="Stay up-to-date!" />
-
-    //   <DropdownCard options={section3Arr} />
-    // </FormCard>
+    <>
+      <TableCard title="Other Community Activities/Events" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfoDropDownSections(closeModal, "Kind of Activity", "Things I Learned", section3Arr)}
+    </>
   )
 }
 
 function section4() {
   const section4Arr = ['Local', 'County', 'Regional', 'State', 'National', 'International'];
   const headers = ["Year", "Kind of Activity", "What I did and time spent", "Region"]
-
   const data = demoData.section4
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Other Community Activities/Events" data={data} headers={headers} />
-    // <FormCard title="Other Community Activities/Events">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <StringCard text="Kind of Activity" name="Soccer Team" />
-    //   <TextAreaCard text="What I did and time spent" string="80 practices, 20 games, 300 hours" />
-      
-    //   <DropdownCard options={section4Arr} />
-    // </FormCard>
+    <>
+      <TableCard title="Other Community Activities/Events" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfoDropDownSections(closeModal, "Kind of Activity", "What I did and time spent", section4Arr)}
+    </>
   )
 }
 
 function section5() {
   const headers = ["Year", "Leadership Title and Responsibilities", "Hours Spent", "Number of People Reached"]
   const data = demoData.section5
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Leadership in 4-H" data={data} headers={headers} />
-    // <FormCard title="Leadership in 4-H">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <TextAreaCard text="Leadership Title and Responsibilities" string="Refreshment committee - brought snacks to two meetings" />
-
-    //   <NumberCard text="Hours Spent" number="1" />
-    //   <NumberCard text="Number of People Reached" number="14" />
-    // </FormCard>
+    <>
+      <TableCard title="Leadership in Other Organizations" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo4Columns(closeModal, "Leadership Title and Responsibilities", "Hours Spent", "Number of People Reached")}
+    </>
   )
 }
 
 function section6() {
   const headers = ["Year", "Name of Organization", "Leadership Responsibilities", "Hours Spent", "Number of People Reached"]
   const data = demoData.section6
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Leadership in Other Organizations" data={data} headers={headers} />
-    // <FormCard title="Leadership in Other Organizations">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <StringCard text="Name of Organization" name="Middle School" />
-    //   <TextAreaCard text="Leadership Responsibilities" string="Student Aid in office; answered phone; greeted visitors" />
-      
-    //   <NumberCard text="Hours Spent" number="50" />
-    //   <NumberCard text="Number of People Reached" number="1000" />
-    // </FormCard>
+    <>
+      <TableCard title="Leadership in Other Organizations" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo4Columns(closeModal, "Name of Organization", "Leadership Responsibilities", "Hours Spent", "Number of People Reached")}
+    </>
   )
 }
 
 function section7() {
   const headers = ["Year", "What I Did as a Club Member and/or Individual", "Hours Served", "Number of People Reached"]
   const data = demoData.section7
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Citizenship/Community Service in 4-H" data={data} headers={headers} />
-    // <FormCard title="Citizenship/Community Service in 4-H">
-    //   <StringCard text="Year" name="2012-13" />
-      
-    //   <TextAreaCard text="What I Did as a Club Member and/or Individual" string="Visited nursing home; introduced my 4-H dog to 4 elderly patients" />
-      
-    //   <NumberCard text="Hours Served" number="4" />
-    //   <NumberCard text="Number of People Reached" number="15" />
-    // </FormCard>
+    <>
+      <TableCard title="Citizenship/Community Service in 4-H" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo4Columns(closeModal, "What I Did as a Club Member and/or Individual", "Hours Served", "Number of People Reached")}
+    </>      
   )
 }
 
 function section8() {
   const headers = ["Year", "What I Did as an Individual or with Another Group", "Hours Served", "Number of People Reached"]
   const data = demoData.section8
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Other Citizenship/Community Service" data={data} headers={headers} />
-  //   <FormCard title="Other Citizenship/Community Service">
-  //     <StringCard text="Year" name="2012-13" />
+    <>
+      <TableCard title="Other Citizenship/Community Service" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo4Columns(closeModal, "What I Did as an Individual or with Another Group", "Hours Served", "Number of People Reached")}
+    </>
+  )
+}
 
-  //     <TextAreaCard text="What I Did as an Individual or with Another Group" string="Did yard work for elderly neighbors" />
-      
-  //     <NumberCard text="Hours Served" number="5" />
-  //     <NumberCard text="Number of People Reached" number="2" />
-  //   </FormCard>
+function addInfo9And10(closeModal) {
+  return (
+    <FormCard onClose={closeModal}>
+      <StringCard text="Year" />
+
+      <StringCard text="Type of Communication" />
+      <StringCard text="Topic" />
+
+      <NumberCard text="Times Given" />
+
+      <StringCard text="Location" />
+
+      <NumberCard text="Size of Audience" />
+    </FormCard>
   )
 }
 
 function section9() {
   const headers = ["Year", "Type of Communication", "Topic", "Times Given", "Location", "Size of Audience"]
   const data = demoData.section9
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Communications in 4-H" data={data} headers={headers} />
-    // <FormCard title="Communications in 4-H">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <StringCard text="Type of Communication" name="Demonstration" />
-    //   <StringCard text="Topic" name="How to Straighten Material" />
-
-    //   <NumberCard text="Times Given" number="2" />
-
-    //   <StringCard text="Location" name="Club & County" />
-      
-    //   <NumberCard text="Size of Audience" number="73" />
-    // </FormCard>
+    <>
+      <TableCard title="Communications in 4-H" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo9And10(closeModal)}
+    </>
   )
 } 
 
 function section10() {
   const headers = ["Year", "Type of Communication", "Topic", "Times Given", "Location", "Size of Audience"]
   const data = demoData.section10
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Communications in Other Organizations" data={data} headers={headers} />
-    // <FormCard title="Communications in Other Organizations">
-    //   <StringCard text="Year" name="2012-13" />
-    // </FormCard>
+    <>
+      <TableCard title="Communications in Other Organizations" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo9And10(closeModal)}
+    </>
   )
 }
 
 function section11() {
   const headers = ["Year", "Event and Level", "Exhibits or Division Shown", "Ribbon(s) Received or Placings"]
   const data = demoData.section11
+  const [showFormCard, setShowFormCard] = useState(false);
 
   return (
-    <TableCard title="Participation in 4-H Contests/Competitions" data={data} headers={headers} />
-    // <FormCard title="Participation in 4-H Contests/Competitions">
-    //   <StringCard text="Year" name="2012-13" />
-    // </FormCard>
+    <>
+      <TableCard title="Participation in 4-H Contests/Competitions" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && (
+        <FormCard onClose={() => setShowFormCard(false)}>
+          <StringCard text="Year" />
+          <StringCard text="Event and Level" />
+
+          <TextAreaCard text="Exhibits or Division Shown" />
+
+          <StringCard text="Ribbon(s) Received or Placings" />
+        </FormCard>
+      )}
+    </>
   )
 }
 
@@ -321,42 +385,51 @@ function section12() {
   const section12Arr = ['Local', 'County', 'Regional', 'State', 'National', 'International'];
   const headers = ["Year", "Contest/Events", "Recognition Received (if any)", "Region"]
   const data = demoData.section12
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Participation in Other Contests/Competitions" data={data} headers={headers} />
-    // <FormCard title="Participation in Other Contests/Competitions">
-    //   <StringCard text="Year" name="2012-13" />
-      
-    //   <DropdownCard options={section12Arr} />
-    // </FormCard>
+    <>
+      <TableCard title="Participation in Other Contests/Competitions" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfoDropDownSections(closeModal, "Contest/Events", "Recognition Received (if any)", section12Arr)}
+    </>
   )
 }
 
 function section13() {
   const headers = ["Year", "Type of Recognition"]
   const data = demoData.section13
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="4-H Recognition" data={data} headers={headers} />
-    // <FormCard title="4-H Recognition">
-    //   <StringCard text="Year" name="2012-13" />
-
-    //   <TextAreaCard text="Type of Recognition" string="Livestock Advancement Certificate - Step 1" />
-    // </FormCard>
+    <>
+      <TableCard title="4-H Recognition" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo3Columns(closeModal, "Type of Recognition")}
+    </>
   )
 }
 
 function section14() {
   const headers = ["Year", "Type of Recognition"]
   const data = demoData.section14
+  const [showFormCard, setShowFormCard] = useState(false);
+
+  const closeModal = () => {
+    setShowFormCard(false)
+  }
 
   return (
-    <TableCard title="Other Recognition" data={data} headers={headers} />
-    // <FormCard title="Other Recognition">
-    //   <StringCard text="Year" name="2012-13" />
-      
-    //   <TextAreaCard text="Type of Recognition" string="Spelling Champion for Grade 9" />
-    // </FormCard>
+    <>
+      <TableCard title="Other Recognition" data={data} headers={headers} handleClick={() => setShowFormCard(true)} />
+      {showFormCard && addInfo3Columns(closeModal, "Type of Recognition")}
+    </>
   )
 }
 
